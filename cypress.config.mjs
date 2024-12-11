e811ae9819615892f870fd774c2788d09b4a1b99
Buster <yahyemohamed2002@gmail.com>
2024-12-11 10:49:45 -0600

    files

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+import customViteConfig from './vite.config';
 
 export default defineConfig({
   component: {
-    port: 5173,
     devServer: {
       framework: 'react',
       bundler: 'vite',
-      viteConfig,
+      viteConfig: customViteConfig,
     },
+    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
